09bf98311d43f40efc77a404c2bbbe7dde27ccb7
mae taylor <mae@mae.wtf>
2026-03-06 16:55:51 +0000

    fix: webkit crashes under nvidia

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    // mitigate WebKit bugs on NVIDIA under Linux
+    #[cfg(target_os = "linux")]
+    unsafe {
+        std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+        std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
+    }
+
     birdcore::run()
 }
