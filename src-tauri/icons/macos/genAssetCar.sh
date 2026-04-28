eaee86103a62eb6c6a18fd3595eac73c00561bfe
vimae <34780309+vimaexd@users.noreply.github.com>
2026-04-28 12:41:56 +0100

    chore: fix macos app icon

--- src-tauri/icons/macos/genAssetCar.sh
+++ src-tauri/icons/macos/genAssetCar.sh
@@ -1,5 +1,6 @@
-actool ./birdpro-icon-mac.icon --compile ./car --output-format human-readable-text --notices --warnings --errors \
-  --output-partial-info-plist ./birdpro.plist \
+mkdir gen
+actool ./birdpro-icon-mac.icon --compile ./gen --output-format human-readable-text --notices --warnings --errors \
+  --output-partial-info-plist ./gen/birdpro.plist \
   --app-icon birdpro-icon-mac --include-all-app-icons \
   --enable-on-demand-resources NO \
   --target-device mac \
