69acd12097fb600b6fb064763afecfb0d116afee
vimae <mae@mae.wtf>
2026-04-29 19:40:41 +0100

    fix: ssml sanitization on Windows backend

--- src-tauri/src/backends/windows.rs
+++ src-tauri/src/backends/windows.rs
@@ -35,7 +35,7 @@ impl TTSProvider for WindowsTTSProvider {
         let lang = voice.voice.lang.as_ref().unwrap();
         let rate = (voice.rate * 10.0).round() as i32;
 
-        let sanitized_message = sanitize_ssml_message(message);
+        let sanitized_message = sanitize_ssml_message(message.to_string());
 
         let ssml = format!("<speak version=\"1.0\" xmlns=\"http://www.w3.org/2001/10/synthesis\" xml:lang=\"{lang}\">
                 <prosody rate=\"{rate:+}%\">
